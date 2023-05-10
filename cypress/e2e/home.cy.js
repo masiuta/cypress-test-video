5994f14f78b2944a8ab8b54af9c48edda64d109a
Ivan <ivan.masiuta@gmail.com>
2023-05-10 16:53:07 +0300

    refactoring

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -4,8 +4,35 @@ describe('Cine books player', () => {
   const firstSubtitle = 'True!';
   const defaultTime = 5000;
   const timeWaitForSubtitle = 15000;
+  const videoSlider = '.slider-wrapper > .rc-slider > .rc-slider-track';
 
-  it('plays video and find first subtitle', () => {
+  function checkVideoGreaterThanPercentage(elementSelector, percentage) {
+    return cy
+      .get(elementSelector)
+      .should('have.attr', 'style')
+      .and('include', 'width:')
+      .then((styleAttribute) => {
+        const widthMatch = styleAttribute.match(/width: (\d+(?:\.\d+)?)%/);
+        const width = widthMatch ? parseFloat(widthMatch[1]) : null;
+        expect(width).to.be.greaterThan(percentage);
+        return width;
+      });
+  }
+
+  function checkVideoLessThanPercentage(elementSelector, percentage) {
+    return cy
+      .get(elementSelector)
+      .should('have.attr', 'style')
+      .and('include', 'width:')
+      .then((styleAttribute) => {
+        const widthMatch = styleAttribute.match(/width: (\d+(?:\.\d+)?)%/);
+        const width = widthMatch ? parseFloat(widthMatch[1]) : null;
+        expect(width).to.be.lessThan(percentage);
+        return width;
+      });
+  }
+
+  function checkPlayVideo() {
     cy.visit('https://cine-books.com');
     cy.get('.page-format__video-info')
       .find('.title')
@@ -14,38 +41,19 @@ describe('Cine books player', () => {
         expect(title).to.equal(titleText);
       });
 
-    //starts video
     cy.get('.play-block-center').click().wait(defaultTime);
 
-    const videoSlider = '.slider-wrapper > .rc-slider > .rc-slider-track';
-
-    function checkVideoGreaterThanPercentage(elementSelector, percentage) {
-      return cy
-        .get(elementSelector)
-        .should('have.attr', 'style')
-        .and('include', 'width:')
-        .then((styleAttribute) => {
-          const widthMatch = styleAttribute.match(/width: (\d+(?:\.\d+)?)%/);
-          const width = widthMatch ? parseFloat(widthMatch[1]) : null;
-          expect(width).to.be.greaterThan(percentage);
-          return width;
-        });
-    }
-
-    function checkVideoLessThanPercentage(elementSelector, percentage) {
-      return cy
-        .get(elementSelector)
-        .should('have.attr', 'style')
-        .and('include', 'width:')
-        .then((styleAttribute) => {
-          const widthMatch = styleAttribute.match(/width: (\d+(?:\.\d+)?)%/);
-          const width = widthMatch ? parseFloat(widthMatch[1]) : null;
-          expect(width).to.be.lessThan(percentage);
-          return width;
-        });
-    }
-
     checkVideoGreaterThanPercentage(videoSlider, 0.15);
+  }
+
+  function checkStopVideo() {
+    cy.get('.play-block-center').click().wait(defaultTime);
+
+    checkVideoLessThanPercentage(videoSlider, 2.4);
+  }
+
+  it('plays video and find first subtitle', () => {
+    checkPlayVideo();
 
     cy.wait(timeWaitForSubtitle);
     cy.get('.video-player-wrapper')
@@ -55,9 +63,6 @@ describe('Cine books player', () => {
         expect(idText).to.equal(firstSubtitle);
       });
 
-    //stops video
-    cy.get('.play-block-center').click().wait(defaultTime)
-
-    checkVideoLessThanPercentage(videoSlider, 2.4)
+    checkStopVideo();
   });
 });
